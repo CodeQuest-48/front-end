8b21df878558554a432f7af550904c0a91e77067
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-03-14 20:53:27 -0500

    rutas de la aplicación actualizadas

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,37 +1,37 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { AuthLayout, HomeLayout } from '../layouts';
-import { LoginPage, NotFoundPage, RegisterPage } from '../pages';
+import { CrearSorteoPage, LoginPage, NotFoundPage, RegisterPage } from '../pages';
 import { enlacesMenu } from '../utils/enlaces';
 
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <HomeLayout />,
-		errorElement: <NotFoundPage />,
-		children: [
-			...enlacesMenu.map(enlace => ({
-				path: enlace.to,
-				element: enlace.component,
-			})),
-			{
-				path: '/',
-				element: <Navigate to={"auth"} />,
-			},
-			{
-				path: 'auth',
-				element: <AuthLayout />,
-				children: [
-					{
-						index: true,
-						path: 'login',
-						element: <LoginPage />,
-					},
-					{
-						path: 'register',
-						element: <RegisterPage />,
-					},
-				],
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      ...enlacesMenu.map((enlace) => ({
+        path: enlace.to,
+        element: enlace.component,
+      })),
+      {
+        path: '/sorteo/nuevo',
+        element: <CrearSorteoPage />,
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+    ],
+  },
 ]);
